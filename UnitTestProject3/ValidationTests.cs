7945238a8e10aff0a85ab430cb2d40ad3aa0a55d
wheesko <41743424+wheesko@users.noreply.github.com>
2018-10-22 22:01:58 +0300

    Expanded unit tests

--- UnitTestProject3/ValidationTests.cs
+++ UnitTestProject3/ValidationTests.cs
@@ -13,5 +13,50 @@ namespace UnitTestProject3
             string result = new DateSeer.Validation().validateRegistrationForm("","","","","",DateTime.Today,true,false);
             Assert.AreEqual(expected, result);
         }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            string expected = "";
+            string name = "Bonifacija";
+            string username = "Bonifaciuxx23";
+            string pass = "Asgrazuole123^";
+            string repeatedPass = "Asgrazuole123^";
+            string email = "grazuole.asEsu@gmail.com";
+            DateTime date = new DateTime(1998,12,9);
+            bool male = false;
+            bool female = true;
+            var result = new DateSeer.Validation().validateRegistrationForm(username, pass, repeatedPass, email, name, date, male, female);
+            Assert.AreEqual(expected, result);
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            string expected = "Email not valid" + Environment.NewLine;
+            string name = "Bonifacija";
+            string username = "Bonifaciuxx23";
+            string pass = "Asgrazuole123^";
+            string repeatedPass = "Asgrazuole123^";
+            string email = "cia lievs emails";
+            DateTime date = new DateTime(1998, 12, 9);
+            bool male = false;
+            bool female = true;
+            var result = new DateSeer.Validation().validateRegistrationForm(username, pass, repeatedPass, email, name, date, male, female);
+            Assert.AreEqual(expected, result);
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+            string expected = "Passwords did not match"+Environment.NewLine+"Email not valid"+Environment.NewLine;
+            string name = "Bonifacija";
+            string username = "Bonifaciuxx23";
+            string pass = "Asgrazuole123^";
+            string repeatedPass = "Asgrazuole123";
+            string email = "grazuole";
+            DateTime date = new DateTime(1998, 12, 9);
+            bool male = false;
+            bool female = true;
+            var result = new DateSeer.Validation().validateRegistrationForm(username, pass, repeatedPass, email, name, date, male, female);
+            Assert.AreEqual(expected, result);
+        }
     }
 }
