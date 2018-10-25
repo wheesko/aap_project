c06fdcbcd35703a3eed6fd497ebd2049c24639a3
wheesko <41743424+wheesko@users.noreply.github.com>
2018-10-25 23:04:58 +0300

    Added DataController class

---
+++ DateSeer/DataController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateSeer
+{
+    public class DataController
+    {
+        private IDataManager dataManager;
+        public DataController(IDataManager gotDataManager)
+        {
+            dataManager = gotDataManager;
+        }
+        public void WriteData(string writeType, Dictionary<string, string> data)
+        {
+            dataManager.WriteData(writeType, data);
+        }
+        public DataTable GetData(string readType)
+        {
+            return dataManager.GetData(readType);
+        }
+    }
+}
